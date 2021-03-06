bbdfb90ee1186d8388d441c758574603563450ce
Justus Rossmeier <veecue@ventos.tk>
2021-03-06 16:41:38 +0100

    implement actual game logic

--- server/game/game.go
+++ server/game/game.go
@@ -24,6 +24,14 @@ var DefaultConfig protocol.GameConfig = protocol.GameConfig{
 	SuggestionTime: 180,
 }
 
+func genToken() string {
+	tok, err := uuid.NewRandom()
+	if err != nil {
+		panic(err)
+	}
+	return tok.String()
+}
+
 func encode(msg protocol.ServerToClient) []byte {
 	ret, err := bare.Marshal(&msg)
 	if err != nil {
@@ -41,21 +49,25 @@ type player struct {
 
 // Game holds the state of a game. Players, board, etc
 type Game struct {
-	Broker      *message.Broker
-	bowlFull    []string
-	bowlCurrent []string
-	players     []*player
-	state       protocol.GameStatus
-	config      protocol.GameConfig
-	timer       chan func()
+	Broker        *message.Broker
+	bowlFull      []string
+	bowlCurrent   []string
+	players       []*player
+	state         protocol.GameStatus
+	config        protocol.GameConfig
+	timer         chan func()
+	playerQueues  map[protocol.Team][]*player
+	currentPlayer *player
+	lastWordToken string
 }
 
 func NewGame() *Game {
 	return &Game{
-		Broker: message.NewBroker(),
-		state:  protocol.Lobby,
-		config: DefaultConfig,
-		timer:  make(chan func()),
+		Broker:       message.NewBroker(),
+		state:        protocol.Lobby,
+		config:       DefaultConfig,
+		timer:        make(chan func()),
+		playerQueues: make(map[protocol.Team][]*player),
 	}
 }
 
@@ -72,6 +84,9 @@ func (g *Game) RunHandler() {
 			err := g.handleMessage(msg)
 			if err != nil {
 				log.Printf("Error handling client message: %v", err)
+				msg.Reply(encode(protocol.Error{
+					Message: err.Error(),
+				}))
 			}
 		case connID := <-g.Broker.ClientsLeaveChan():
 			for _, p := range g.players {
@@ -124,10 +139,7 @@ func (g *Game) handleClientHello(msg *protocol.ClientHello, orig message.ToServe
 		}
 	}
 	id := len(g.players)
-	token, err := uuid.NewRandom()
-	if err != nil {
-		return fmt.Errorf("Error generating token: %w", err)
-	}
+	token := genToken()
 	team := protocol.Red
 	if rand.Intn(2) == 0 {
 		team = protocol.Blue
@@ -141,10 +153,10 @@ func (g *Game) handleClientHello(msg *protocol.ClientHello, orig message.ToServe
 			Team:   team,
 		},
 		connection: orig.ClientID,
-		token:      token.String(),
+		token:      token,
 	})
 	orig.Reply(encode(protocol.ServerHello{
-		Token:    token.String(),
+		Token:    token,
 		PlayerID: id,
 	}))
 	g.broadcastPlayerList()
@@ -172,6 +184,57 @@ func (g *Game) broadcastBowlState() {
 	}))
 }
 
+func (g *Game) nextWord() {
+	if len(g.bowlCurrent) == 0 {
+		return
+		// TODO cancel the timeout early
+	}
+
+	word := g.bowlCurrent[0]
+	g.bowlCurrent = g.bowlCurrent[1:]
+	g.broadcastBowlState()
+
+	g.lastWordToken = genToken()
+	g.Broker.SendToClient(g.currentPlayer.connection, encode(protocol.WordNew{
+		Token:    g.lastWordToken,
+		Word:     word,
+		TimeLeft: 30, // TODO
+	}))
+}
+
+func (g *Game) nextPlayer() {
+	if len(g.bowlCurrent) == 0 {
+		g.startRound()
+	}
+
+	// decide the team of the next player
+	team := protocol.Red
+	if g.currentPlayer != nil && g.currentPlayer.Team == protocol.Red {
+		team = protocol.Blue
+	}
+
+	// if no players are in queue for the desired team, refill the queues
+	if len(g.playerQueues[team]) == 0 {
+		for _, p := range g.players {
+			if p.Team == team {
+				g.playerQueues[team] = append(g.playerQueues[team], p)
+			}
+		}
+	}
+
+	if g.currentPlayer != nil {
+		g.currentPlayer.Status = protocol.Passive
+	}
+
+	g.currentPlayer = g.playerQueues[team][0]
+	g.playerQueues[team] = g.playerQueues[team][1:]
+
+	g.setTimer(30*time.Second, g.nextPlayer)
+	g.currentPlayer.Status = protocol.Active
+	g.broadcastPlayerList()
+	g.nextWord()
+}
+
 func (g *Game) startRound() {
 	g.bowlCurrent = g.bowlCurrent[:0]
 	g.bowlCurrent = append(g.bowlCurrent, g.bowlFull...)
@@ -179,7 +242,7 @@ func (g *Game) startRound() {
 		g.bowlCurrent[i], g.bowlCurrent[j] = g.bowlCurrent[j], g.bowlCurrent[i]
 	})
 	g.broadcastBowlState()
-	// TODO
+	g.nextPlayer()
 }
 
 func (g *Game) handleWordSuggestions(msg *protocol.WordSuggestions, orig message.ToServerMsg) error {
@@ -223,6 +286,16 @@ func (g *Game) handleUpdatePlayerInfo(msg *protocol.UpdatePlayerInfo, orig messa
 		p.Team = *msg.Team
 	}
 	g.broadcastPlayerList()
+	return nil
+}
+
+func (g *Game) handleWordSuccess(msg *protocol.WordSuccess, orig message.ToServerMsg) error {
+	if g.lastWordToken == "" || msg.Token != g.lastWordToken {
+		return errors.New("Invalid token")
+	}
+	g.currentPlayer.Score++
+	g.broadcastPlayerList()
+	g.nextWord()
 	return nil
 }
 
