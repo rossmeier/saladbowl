4d229cc6ae060cc4d88304bab94f39846e6b2fd1
Tibor Aufdemkamp <tiboradk@users.noreply.github.com>
2021-03-08 20:22:01 +0100

    Configuration of a game can now be edited in the user lobby. This functionality is currently limited to the owner of the game, therefore the panel will not be shown until the Player struct will be extended to include an property `owner` with type `boolean`. In the client `UserType` representing Player has been extended with an optional property `owner:boolean`.

--- client/saladbowl-client/src/components/Lobby.tsx
+++ client/saladbowl-client/src/components/Lobby.tsx
@@ -1,32 +1,20 @@
 import {
-    Button, Container,
+    Button,
+    Container,
     FormControl,
     FormControlLabel,
-    FormLabel, Grid, Paper,
+    FormLabel,
+    Grid,
+    InputAdornment,
+    Paper,
     Radio,
     RadioGroup,
-    TextField
+    TextField,
+    Typography
 } from "@material-ui/core";
-import React, {useState} from "react";
+import React, {useReducer, useState} from "react";
 import {Team, UserMapType, UsersList, UserType} from "./User";
 
-function ChooseTeam({disabled, team, teams}: { team: Team, teams: Team[], disabled?: boolean, }) {
-    const [value, setValue] = useState(team);
-
-    const buttons = teams.map(team => <FormControlLabel value={team} control={<Radio/>} label={Team[team]}
-                                                        disabled={disabled}/>)
-
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setValue(parseInt(event.target.value));
-    }
-
-    return <FormControl component="fieldset">
-        <FormLabel component="legend">Team</FormLabel>
-        <RadioGroup row arial-label="team" name="team" value={value} onChange={handleChange}>
-            {buttons}
-        </RadioGroup>
-    </FormControl>
-}
 
 function UserLobby(props: { joinGame: (username: string) => void }) {
     const [name, setName] = useState(sessionStorage.getItem('username') ?? '');
@@ -50,7 +38,112 @@ function UserLobby(props: { joinGame: (username: string) => void }) {
     </div>
 }
 
-function PlayerLobby(props: { user: UserType, teams: Team[] }) {
+function ChooseTeam({disabled, team, teams}: { team: Team, teams: Team[], disabled?: boolean, }) {
+    const [value, setValue] = useState(team);
+
+    const buttons = teams.map(team => <FormControlLabel key={team} value={team} control={<Radio/>} label={Team[team]}
+                                                        disabled={disabled}/>)
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(parseInt(event.target.value));
+    }
+
+    return <FormControl component="fieldset">
+        <FormLabel component="legend">Team</FormLabel>
+        <RadioGroup row arial-label="team" name="team" value={value} onChange={handleChange}>
+            {buttons}
+        </RadioGroup>
+    </FormControl>
+}
+
+function GameConfig(props: { defaults: { maxWords: number, suggestionTime: number, guessingTime: number, rounds: number }, onSubmit: (maxWords: number, suggestionTime: number, guessingTime: number, rounds: number) => void }) {
+    const [values, setValues] = useState(props.defaults);
+
+    function errorReducer(state: { maxWords: string, suggestionTime: string, guessingTime: string, rounds: string }, action: { nam: string, val: number }): { maxWords: string; suggestionTime: string; guessingTime: string; rounds: string } {
+        let msg;
+        if (isNaN(action.val)) {
+            msg = 'Not a umber';
+        } else if (action.val < 0) {
+            msg = 'Must be greater than 0';
+        } else {
+            msg = '';
+        }
+
+        return {...state, [action.nam]: msg}
+    }
+
+    const [errors, dispatchErrors] = useReducer(errorReducer, {
+        maxWords: '',
+        suggestionTime: '',
+        guessingTime: '',
+        rounds: ''
+    });
+
+    const {
+        guessingTime: guessingTimeDefault,
+        rounds: roundsDefault,
+        maxWords: maxWordsDefault,
+        suggestionTime: suggestionTimeDefault
+    } = props.defaults;
+    const {guessingTime, rounds, maxWords, suggestionTime} = values;
+
+
+    const noErrors = () => {
+        return Object.values(errors).reduce((pre, cur) => pre && cur === '', true);
+    }
+
+    const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+        const nam = event.target.name;
+        let val = parseInt(event.target.value);
+        val = isNaN(val) || val < 0 ? 0 : val;
+        dispatchErrors({nam, val});
+
+        setValues({...values, [nam]: isNaN(val) ? undefined : val});
+    }
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        if (noErrors()) {
+            props.onSubmit(maxWords, suggestionTime, guessingTime, rounds);
+        }
+    }
+
+    const iaSeconds = <InputAdornment position="end">s</InputAdornment>
+    const buttonDisabled = () => {
+        return maxWordsDefault === maxWords && suggestionTimeDefault === suggestionTime && guessingTimeDefault === guessingTime && roundsDefault === rounds;
+    }
+
+
+    return <Paper>
+        <Typography variant="subtitle1">Game:</Typography>
+        <form onSubmit={handleSubmit} autoComplete="off" onError={() => console.log('error')}>
+            <TextField id="input-maxWords" label="Words to submit" type="number" name="maxWords"
+                       required fullWidth
+                       value={maxWords ?? ''} onChange={handleChange} placeholder={maxWordsDefault + ''}
+                       error={errors.maxWords !== ''} helperText={errors.maxWords}/>
+            <br/>
+            <TextField id="input-suggestionTime" label="Suggestion Time" type="number" name="suggestionTime"
+                       required fullWidth InputProps={{endAdornment: iaSeconds}}
+                       value={suggestionTime ?? ''} onChange={handleChange}
+                       placeholder={suggestionTimeDefault + ''}
+                       error={errors.suggestionTime !== ''} helperText={errors.suggestionTime}/>
+            <br/>
+            <TextField id="input-guessingTime" label="Time to guess" type="number" name="guessingTime"
+                       required fullWidth disabled InputProps={{endAdornment: iaSeconds}}
+                       value={guessingTime ?? ''} onChange={handleChange} placeholder={guessingTimeDefault + ''}
+                       error={errors.guessingTime !== ''} helperText={errors.guessingTime}/>
+            <br/>
+            <TextField id="input-rounds" label="Rounds to play" type="number" name="rounds"
+                       required fullWidth
+                       value={rounds ?? ''} onChange={handleChange} placeholder={roundsDefault + ''}
+                       error={errors.rounds !== ''} helperText={errors.rounds}/>
+            <br/>
+            <Button type="submit" disabled={buttonDisabled() || !noErrors()}>Set</Button>
+        </form>
+    </Paper>
+}
+
+function PlayerLobby(props: { user: UserType, teams: Team[], onReady: (name: string, team: Team) => void, onStart: () => void , onConfigSubmit:  (maxWords: number, suggestionTime: number, guessingTime: number, rounds: number) => void}) {
     const {user, teams} = props;
 
     const [ready, setReady] = useState(false);
@@ -60,34 +153,49 @@ function PlayerLobby(props: { user: UserType, teams: Team[] }) {
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         setReady(!ready);
+        props.onReady(name, user.team);
     }
 
     const handleNameChange = (event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
         setName(event.target.value);
     }
 
-    return <div className="PlayerLobby">
-        <form onSubmit={handleSubmit}>
-            <TextField id="input-username" label="Username" value={name} required onChange={handleNameChange}
-                       disabled={ready}/>
-            <br/>
-            <ChooseTeam team={user.team} teams={teams} disabled={ready}/>
-            <br/>
-            <Button type="submit">{ready ? "Edit" : "Ready"}</Button>
-        </form>
-        <Button>StartGame</Button>
-    </div>
+    // TODO set disabled to `ready` when ability to change team is implemented
+    return (
+        <Grid container className="PlayerLobby" direction="column" spacing={2}>
+            <Grid item>
+                { user.owner ? <GameConfig defaults={{guessingTime: 30, maxWords: 15, suggestionTime: 180, rounds: 3}}
+                            onSubmit={props.onConfigSubmit}/> : null }
+            </Grid>
+            <Grid item>
+                <Paper>
+                    <Typography variant="subtitle1">User Settings:</Typography>
+
+                    <form onSubmit={handleSubmit}>
+                        <TextField id="input-username" label="Username" value={name} required
+                                   onChange={handleNameChange}
+                                   disabled={ready}/>
+                        <br/>
+                        <ChooseTeam team={user.team} teams={teams} disabled/>
+                        <br/>
+                        <Button type="submit">{ready ? "Edit" : "Ready"}</Button>
+                    </form>
+                </Paper>
+            </Grid>
+            <Button>StartGame</Button>
+        </Grid>
+    );
 }
 
-function Lobby(props: { user?: UserType, users: UserMapType, joinGame: (name: string) => void }) {
-    const {user, users} = props;
+function Lobby(props: { user?: UserType, users: UserMapType, joinGame: (name: string) => void, onReady: (name: string, team: Team) => void, onStart: () => void , onConfigSubmit: (maxWords: number, suggestionTime: number, guessingTime: number, rounds: number) => void}) {
+    const {user, users, joinGame, onReady, onStart, onConfigSubmit} = props;
 
     let left;
     if (user) {
         users.delete(user.id);
-        left = <PlayerLobby user={user} teams={[Team.BLUE, Team.RED]}/>
+        left = <PlayerLobby user={user} teams={[Team.BLUE, Team.RED]} onReady={onReady} onStart={onStart} onConfigSubmit={onConfigSubmit}/>
     } else {
-        left = <UserLobby joinGame={props.joinGame}/>
+        left = <UserLobby joinGame={joinGame}/>
     }
 
     return (
--- client/saladbowl-client/src/components/SaladBowl.tsx
+++ client/saladbowl-client/src/components/SaladBowl.tsx
@@ -1,21 +1,9 @@
 import Lobby from "./Lobby";
-import {UserType} from "./User";
+import {Team, UserType} from "./User";
 import {useEffect, useState} from "react";
-import {ClientHello, ClientToServer} from "../protocol/messages";
-import {UnionValue} from "../protocol/bare";
 import Client from "../protocol/MessageHandler";
 import useWebsocket from "../hooks/WebsocketHook";
 
-
-const dummyUser = new Map<number, UserType>(
-    Array.from([0, 1, 2, 3, 4, 5].map(id => [id, {
-        name: 'User' + id * 3,
-        id: id,
-        team: Math.floor((Math.random() * 10) % 2),
-        status: Math.floor((Math.random() * 10) % 3),
-        score: id * Math.floor(Math.random() * 1000)
-    }]))
-);
 
 const host = window.location.host
 const hostname = window.location.hostname
@@ -23,13 +11,13 @@ const port: number | undefined = 8080;
 
 function SaladBowl({token: gameToken}: { token?: string | null }) {
 
-    const [playerID, setPlayerID] = useState(NaN);
+    const [playerID, setPlayerID] = useState(parseInt(sessionStorage.getItem('playerID') ?? ''));
     const [token, setToken] = useState(sessionStorage.getItem('userToken'));
     const [users, setUsers] = useState(new Map<number, UserType>());
     const [messageHandler, setMessageHandler] = useState(new Client());
 
     const ws = useWebsocket({
-        socketUrl: `ws://${port ? hostname : host}${port ? ':' + port : ''}/ws${gameToken ? '/'+gameToken : ''}`,
+        socketUrl: `ws://${port ? hostname : host}${port ? ':' + port : ''}/ws${gameToken ? '/' + gameToken : ''}`,
         retry: 3,
     });
 
@@ -60,11 +48,14 @@ function SaladBowl({token: gameToken}: { token?: string | null }) {
     }, [token])
 
     useEffect(() => {
-        messageHandler.onServerHello = value => {
-            setToken(value.token);
-            setPlayerID(value.playerID);
-        }
+        messageHandler.onServerHello = ({playerID: _playerID, token: _token}) => {
+            console.log(_playerID + ': ' + _token);
+            sessionStorage.setItem('playerID', String(_playerID))
+            setToken(_token);
+            setPlayerID(Number(_playerID));
+        };
         messageHandler.onPlayerList = value => {
+            console.log('received PlayerList');
             // We need to make sure newUser is interpreted as array of tuples [any, any][] and not as array of arrays (type1 | type2)[][].
             // The second value is of type any so we don't need to import PlayerValue from th MessageHandler
             // TODO Type correctly, after merging types from User.tsx and MessageHandler.ts to a single location.
@@ -73,16 +64,28 @@ function SaladBowl({token: gameToken}: { token?: string | null }) {
         }
     }, [messageHandler]);
 
-    const joinGame = (username: string) => {
-        console.log('joining game as user ' + username);
-        const obj = new UnionValue(ClientHello, {name: username});
-        ws.send(ClientToServer.pack(obj));
+    const joinGame = (name: string) => {
+        console.log('joining game as user ' + name);
+        ws.send(messageHandler.clientHello(name, token ?? undefined));
     }
 
+    const updateGameConfig = (words: number, suggT: number, guessT: number, rounds: number) => {
+        console.log('Updating gameconfig', words, suggT, guessT, rounds);
+    }
+
+    const updatePlayerInfo = (name: string, team: Team) => {
+        console.log('Updating player info: ' + name + ' ' + team);
+        ws.send(messageHandler.updatePlayerInfo(name, team));
+    }
+
+    const startGame = () => {
+        console.log('send start game');
+        ws.send(messageHandler.startGame());
+    }
     const user = users.get(playerID);
 
     return <div className="SaladBowl">
-        <Lobby user={user} users={dummyUser} joinGame={joinGame}/>
+        <Lobby user={user} users={new Map(users)} joinGame={joinGame} onStart={startGame} onReady={updatePlayerInfo} onConfigSubmit={updateGameConfig}/>
     </div>
 }
 
